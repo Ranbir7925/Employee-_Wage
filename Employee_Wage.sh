7880114d57556c19da5e77efad2b9f62ed4ca46d
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 18:33:00 +0530

    Added part time employee and checked its wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -9,16 +9,17 @@ Part_Time_Hrs=4
 if [ $Emp_check -eq 1 ]
 then
   echo "Employee Is Present..."
-
   Wage=$(( Wage_Per_Hr * Full_Day_Hrs ))
   echo "Total Wage is $Wage "
 
 elif [ $Emp_check -eq 2 ]
 then
+
   echo "Employee Is Part Time Present..."
   Wage=$(( Wage_Per_Hr * Part_Time_Hrs ))
   echo "Part Time Wage is $Wage "
 
 else
   echo "Employee Is Not Present..."
+
 fi
