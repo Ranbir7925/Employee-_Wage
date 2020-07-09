c52fcfc28256a545554294afbd4064ce09479c18
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 20:50:39 +0530

    Calculated Wages for total hrs and months

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,35 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Employee_Wage Computation Program"
-
+Is_full_time=1
+Is_part_time=2
+max_hrs_in_month=10
 Wage_Per_Hr=20
 Working_days_per_month=20
 
+Total_emp_hrs=0;
+Total_working_days=0;
 Emp_check=$(( RANDOM %3 ))
+while [[ $Total_emp_hrs -lt $max_hrs_in_month && $Total_working_days -lt $working_days_per_month ]]
+do
+	((Total_working_days++))
+	case $Emp_check in
+		$Is_full_time)
+			Emp_Hrs=8
+			;;
 
-case $Emp_check in
-  1)
-  echo "Employee Is Present..."
-  Emp_Hrs=8
-  ;;
+		$Is_part_time)
+			Emp_Hrs=4
+			;;
 
-  2)
-  echo "Employee Is Part Time Present..."
-  Emp_Hrs=4
-  ;;
-
-  0)
-  echo "Employee Is Not Present..."
-  Emp_Hrs=0
-  ;;
-
-  *)
-  echo "INVALID"
-  ;;
-esac
-
-Daily_Wage=$(( Wage_Per_Hr * Emp_Hrs ))
-Total_Wage=$(( Daily_Wage * Working_days_per_month ))
-
-echo "Daily Wage is=$Daily_Wage"
-echo "Monthly Wage is=$Total_Wage"
+		*)
+			Emp_Hrs=0
+			;;
+	esac
+	Total_emp_hrs=$((Total_emp_hrs+Emp_Hrs))
+done
+Total_Salary=$((Total_emp_hrs*Wage_per_hr))
