c128b762f943e99454cfe8f811071e3c43754b14
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 18:18:38 +0530

    Calculated Daily Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -3,10 +3,13 @@
 echo "Welcome to Employee_Wage Computation Program"
 
 Emp_check=$(( RANDOM %2 ))
-
+WagePerHr=20
 if [ $Emp_check -eq 1 ]
 then
   echo "Employee Is Present..."
+  FullDayHrs=8
+  Wage=$(( WagePerHr * FullDayHrs ))
+  echo "Total Wage is $Wage "
 else
   echo "Employee Is Not Present..."
 fi
