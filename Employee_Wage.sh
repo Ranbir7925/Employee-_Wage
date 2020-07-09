f3d3737aa6cc2328d2c24390ec322d3449c0ed9c
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 18:05:44 +0530

    Employee checking is done

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to Employee_Wage Computation Program"
+
+Emp_check=$(( RANDOM %2 ))
+
+if [ $Emp_check -eq 1 ]
+then
+  echo "Employee Is Present..."
+else
+  echo "Employee Is Not Present..."
+fi
