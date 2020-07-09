818372ba0d9a5b3b09a51ea3a805c5d2214c8f68
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 22:45:04 +0530

    Store Day

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -8,8 +8,10 @@ num_working_day=20
 total_emphr=0
 total_working_days=0
 
+declare -A dailyWages
+
 function getworkhr() {
-local $empcheck=$1
+        local $empcheck=$1
         case $empcheck in
                 $is_full_time)
                         emphrs=8 ;;
@@ -21,6 +23,10 @@ local $empcheck=$1
         echo $emphrs
 }
 
+function getEmpWage() {
+        local emphrs=$1
+        echo $(($emphrs*$emp_rate_per_hr))
+}
 
 while [[ $total_emphr -lt $max_hrs_in_month && $total_working_days -lt $num_working_day ]]
 do
@@ -28,10 +34,9 @@ do
         empcheck=$((RANDOM%3))
         emphrs="$( getworkhr $empcheck )"
         total_emphr=$(($total_emphr+$emphrs))
-        dailyWages[$total_working_days]=$(($emphrs*$emp_rate_per_hr))
+        dailyWages["Day "$total_working_days]="$( getEmpWage $emphrs)"
 done
 
 totalSalary=$(($total_emphr*$emp_rate_per_hr))
 echo ${dailyWages[@]}
-
-
+echo ${!dailyWages[@]}
