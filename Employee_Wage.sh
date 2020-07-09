3c5c7c6af6933baed008f03c8a32a86af65982f3
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 19:07:18 +0530

    Calculated Wages for a month

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -2,29 +2,34 @@
 
 echo "Welcome to Employee_Wage Computation Program"
 
-Emp_check=$(( RANDOM %3 ))
 Wage_Per_Hr=20
-Full_Day_Hrs=8
-Part_Time_Hrs=4
+Working_days_per_month=20
+
+Emp_check=$(( RANDOM %3 ))
 
 case $Emp_check in
   1)
   echo "Employee Is Present..."
-  Wage=$(( Wage_Per_Hr * Full_Day_Hrs ))
-  echo "Total Wage is $Wage "
+  Emp_Hrs=8
   ;;
 
   2)
   echo "Employee Is Part Time Present..."
-  Wage=$(( Wage_Per_Hr * Part_Time_Hrs ))
-  echo "Part Time Wage is $Wage "
+  Emp_Hrs=4
   ;;
 
   0)
   echo "Employee Is Not Present..."
+  Emp_Hrs=0
   ;;
 
   *)
   echo "INVALID"
   ;;
 esac
+
+Daily_Wage=$(( Wage_Per_Hr * Emp_Hrs ))
+Total_Wage=$(( Daily_Wage * Working_days_per_month ))
+
+echo "Daily Wage is=$Daily_Wage"
+echo "Monthly Wage is=$Total_Wage"
