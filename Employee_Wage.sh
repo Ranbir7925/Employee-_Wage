f86a633f0a61c5bfb17130135a5f4bc2a9acdbcb
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 21:42:54 +0530

    code Refactoring is done

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -3,29 +3,35 @@
 echo "Welcome to Employee_Wage Computation Program"
 Is_full_time=1
 Is_part_time=2
-max_hrs_in_month=10
+max_hrs_in_month=4
 Wage_Per_Hr=20
 Working_days_per_month=20
 
 Total_emp_hrs=0;
 Total_working_days=0;
-Emp_check=$(( RANDOM %3 ))
-while [[ $Total_emp_hrs -lt $max_hrs_in_month && $Total_working_days -lt $working_days_per_month ]]
-do
-	((Total_working_days++))
-	case $Emp_check in
-		$Is_full_time)
-			Emp_Hrs=8
-			;;
 
-		$Is_part_time)
-			Emp_Hrs=4
-			;;
+function getWorkHrs() {
+	case $Emp_check in
+        	$Is_full_time)
+			Emp_Hrs=8
+                	;;
+
+        	$Is_part_time)
+                	Emp_Hrs=4
+                	;;
 
 		*)
-			Emp_Hrs=0
-			;;
+                	Emp_Hrs=0
+                	;;
 	esac
-	Total_emp_hrs=$((Total_emp_hrs+Emp_Hrs))
+	echo $Emp_Hrs
+}
+
+while [[ ($Total_emp_hrs -lt $max_hrs_in_month) && ($Total_working_days -lt $Working_days_per_month) ]]
+do
+	((Total_working_days++))
+	Emp_check=$((RANDOM%3))
+	Emp_hrs="$( getWorkHrs $Emp_check )"
+	Total_emp_hrs=$(($Total_emp_hrs+$Emp_Hrs))
 done
-Total_Salary=$((Total_emp_hrs*Wage_per_hr))
+Total_Salary=$(($Total_emp_hrs*$Wage_per_hr))
