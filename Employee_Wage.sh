ca67ed86e5f2896cb269c74f9d6ba5d5179f7606
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-09 18:28:15 +0530

    Added part time employee and checked its wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -2,14 +2,23 @@
 
 echo "Welcome to Employee_Wage Computation Program"
 
-Emp_check=$(( RANDOM %2 ))
-WagePerHr=20
+Emp_check=$(( RANDOM %3 ))
+Wage_Per_Hr=20
+Full_Day_Hrs=8
+Part_Time_Hrs=4
 if [ $Emp_check -eq 1 ]
 then
   echo "Employee Is Present..."
-  FullDayHrs=8
-  Wage=$(( WagePerHr * FullDayHrs ))
+
+  Wage=$(( Wage_Per_Hr * Full_Day_Hrs ))
   echo "Total Wage is $Wage "
+
+elif [ $Emp_check -eq 2 ]
+then
+  echo "Employee Is Part Time Present..."
+  Wage=$(( Wage_Per_Hr * Part_Time_Hrs ))
+  echo "Part Time Wage is $Wage "
+
 else
   echo "Employee Is Not Present..."
 fi
